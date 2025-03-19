a39af65c1c7a0a96c5b772a0361e390c4b1d6789
hrmedi-dev <137749005+hrmedi-dev@users.noreply.github.com>
2025-03-19 04:45:40 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,15 +2,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/get', methods=['GET'])
-def get_example():
-    return jsonify({'message': 'GET request received'}), 200
+@app.route('/data', methods=['GET'])
+def get_data():
+    # Responde con un JSON de ejemplo
+    return jsonify({"message": "Hello, this is a GET response!"})
 
-@app.route('/post', methods=['POST'])
-def post_example():
-    data = request.get_json()  # Obtiene los datos enviados en el cuerpo del POST
-    return jsonify({'message': 'POST request received', 'data': data}), 200
+@app.route('/data', methods=['POST'])
+def post_data():
+    # Obtiene los datos enviados en la solicitud POST
+    data = request.get_json()
+    temperature = data.get('temperature')
+    humidity = data.get('humidity')
+    
+    # Responde con los datos recibidos
+    return jsonify({
+        "received": {
+            "temperature": temperature,
+            "humidity": humidity
+        }
+    })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
-
