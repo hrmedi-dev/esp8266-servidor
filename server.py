7c60f0b079164dee7e92c6fb796fbe0facf5b4b1
hrmedi <137749005+hrmedi-dev@users.noreply.github.com>
2025-03-19 22:17:23 -0300

    Update server.py

--- server.py
+++ server.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 @app.route('/data', methods=['GET'])
 def get_data():
     # Responde con un JSON de ejemplo
-    return jsonify({"message": "Hola, prueba hrm!"})
+    return jsonify({"message": "Hello, this is a GET response!"})
 
 @app.route('/data', methods=['POST'])
 def post_data():
     # Obtiene los datos enviados en la solicitud POST
     data = request.get_json()
-    temperatura = data.get('temperatura')
-    humedad = data.get('humedad')
+    temperature = data.get('temperature')
+    humidity = data.get('humidity')
     
     # Responde con los datos recibidos
     return jsonify({
         "received": {
-            "temperatura": temperatura,
-            "humedad": humedad
+            "temperature": temperature,
+            "humidity": humidity
         }
     })
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
