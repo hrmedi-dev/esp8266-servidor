f705354376f627ee2c9d7f3cbfbcbd2ee23bfe24
hrmedi <137749005+hrmedi-dev@users.noreply.github.com>
2025-03-20 09:23:47 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,10 +7,10 @@ def get_data():
     # Responde con un JSON de ejemplo
     return jsonify({"message": "Hola, prueba HRM!"})
 
-@app.route('/data', methods=['POST'])
+@app.route('/dato', methods=['POST'])
 def post_data():
     # Obtiene los datos enviados en la solicitud POST
-    data = request.get_json()
+    dato = request.get_json()
     temperature = data.get('temperature')
     humidity = data.get('humidity')
     
